a4dbc383d508760811140b4305cff226ea015cfe
Hasnain <alvihasnain305@gmail.com>
2025-02-19 15:26:55 +0500

    last changes

---
+++ app.js
@@ -0,0 +1,16 @@
+
+let signup = document.getElementById('login');
+signup.addEventListener('click',()=>{
+    window.location.href= "/login.html"
+    console.log("Button Clicked")
+})
+
+let mybutton = document.querySelector('#signup');
+mybutton.addEventListener('click',()=>{
+    window.location.href= "/signup.html"
+
+    console.log("Button Clicked");
+})
+
+
+
